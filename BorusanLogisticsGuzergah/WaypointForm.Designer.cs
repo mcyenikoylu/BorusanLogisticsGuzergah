2948a97292140cf1f9360968269643cc8ed66a43
Mehmet Cenk Yeniköylü <mcy@dotnetmcy.net>
2021-08-25 23:45:05 +0300

    bing maps key

--- BorusanLogisticsGuzergah/WaypointForm.Designer.cs
+++ BorusanLogisticsGuzergah/WaypointForm.Designer.cs
@@ -34,7 +34,23 @@
             this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
             this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
+            this.tabGrid = new DevExpress.XtraTab.XtraTabPage();
+            this.tabMap = new DevExpress.XtraTab.XtraTabPage();
+            this.gridSplitContainer1 = new DevExpress.XtraGrid.GridSplitContainer();
+            this.gridSplitContainer1Grid = new DevExpress.XtraGrid.GridControl();
+            this.gridSplitContainer1View = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.mapControl1 = new DevExpress.XtraMap.MapControl();
             ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
+            this.xtraTabControl1.SuspendLayout();
+            this.tabGrid.SuspendLayout();
+            this.tabMap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSplitContainer1)).BeginInit();
+            this.gridSplitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSplitContainer1Grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSplitContainer1View)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).BeginInit();
             this.SuspendLayout();
             // 
             // ribbon
@@ -48,7 +64,7 @@
             this.ribbon.Name = "ribbon";
             this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.ribbon.Size = new System.Drawing.Size(442, 143);
+            this.ribbon.Size = new System.Drawing.Size(701, 143);
             this.ribbon.StatusBar = this.ribbonStatusBar;
             // 
             // barCheckItem1
@@ -72,17 +88,81 @@
             // 
             // ribbonStatusBar
             // 
-            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 418);
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 496);
             this.ribbonStatusBar.Name = "ribbonStatusBar";
             this.ribbonStatusBar.Ribbon = this.ribbon;
-            this.ribbonStatusBar.Size = new System.Drawing.Size(442, 31);
+            this.ribbonStatusBar.Size = new System.Drawing.Size(701, 31);
             this.ribbonStatusBar.Visible = false;
+            // 
+            // xtraTabControl1
+            // 
+            this.xtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xtraTabControl1.Location = new System.Drawing.Point(0, 143);
+            this.xtraTabControl1.Name = "xtraTabControl1";
+            this.xtraTabControl1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.xtraTabControl1.SelectedTabPage = this.tabMap;
+            this.xtraTabControl1.Size = new System.Drawing.Size(701, 353);
+            this.xtraTabControl1.TabIndex = 2;
+            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.tabGrid,
+            this.tabMap});
+            // 
+            // tabGrid
+            // 
+            this.tabGrid.Controls.Add(this.gridSplitContainer1);
+            this.tabGrid.Name = "tabGrid";
+            this.tabGrid.Size = new System.Drawing.Size(695, 325);
+            this.tabGrid.Text = "Grid";
+            // 
+            // tabMap
+            // 
+            this.tabMap.Controls.Add(this.mapControl1);
+            this.tabMap.Name = "tabMap";
+            this.tabMap.Size = new System.Drawing.Size(695, 325);
+            this.tabMap.Text = "Map";
+            // 
+            // gridSplitContainer1
+            // 
+            this.gridSplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridSplitContainer1.Grid = this.gridSplitContainer1Grid;
+            this.gridSplitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.gridSplitContainer1.Name = "gridSplitContainer1";
+            this.gridSplitContainer1.Panel1.Controls.Add(this.gridSplitContainer1Grid);
+            this.gridSplitContainer1.Panel1.Text = "Panel1";
+            this.gridSplitContainer1.Panel2.Text = "Panel2";
+            this.gridSplitContainer1.Size = new System.Drawing.Size(695, 325);
+            this.gridSplitContainer1.TabIndex = 0;
+            // 
+            // gridSplitContainer1Grid
+            // 
+            this.gridSplitContainer1Grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridSplitContainer1Grid.Location = new System.Drawing.Point(0, 0);
+            this.gridSplitContainer1Grid.MainView = this.gridSplitContainer1View;
+            this.gridSplitContainer1Grid.Name = "gridSplitContainer1Grid";
+            this.gridSplitContainer1Grid.Size = new System.Drawing.Size(695, 325);
+            this.gridSplitContainer1Grid.TabIndex = 0;
+            this.gridSplitContainer1Grid.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridSplitContainer1View});
+            // 
+            // gridSplitContainer1View
+            // 
+            this.gridSplitContainer1View.GridControl = this.gridSplitContainer1Grid;
+            this.gridSplitContainer1View.Name = "gridSplitContainer1View";
+            // 
+            // mapControl1
+            // 
+            this.mapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapControl1.Location = new System.Drawing.Point(0, 0);
+            this.mapControl1.Name = "mapControl1";
+            this.mapControl1.Size = new System.Drawing.Size(695, 325);
+            this.mapControl1.TabIndex = 0;
             // 
             // WaypointForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 449);
+            this.ClientSize = new System.Drawing.Size(701, 527);
+            this.Controls.Add(this.xtraTabControl1);
             this.Controls.Add(this.ribbonStatusBar);
             this.Controls.Add(this.ribbon);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -90,7 +170,17 @@
             this.Ribbon = this.ribbon;
             this.StatusBar = this.ribbonStatusBar;
             this.Text = "Waypoint";
+            this.Load += new System.EventHandler(this.WaypointForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
+            this.xtraTabControl1.ResumeLayout(false);
+            this.tabGrid.ResumeLayout(false);
+            this.tabMap.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridSplitContainer1)).EndInit();
+            this.gridSplitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridSplitContainer1Grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSplitContainer1View)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,5 +193,12 @@
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
         private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
         private DevExpress.XtraBars.BarCheckItem barCheckItem1;
+        private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+        private DevExpress.XtraTab.XtraTabPage tabGrid;
+        private DevExpress.XtraTab.XtraTabPage tabMap;
+        private DevExpress.XtraGrid.GridSplitContainer gridSplitContainer1;
+        private DevExpress.XtraGrid.GridControl gridSplitContainer1Grid;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridSplitContainer1View;
+        private DevExpress.XtraMap.MapControl mapControl1;
     }
 }
--- BorusanLogisticsGuzergah/WaypointForm.cs
+++ BorusanLogisticsGuzergah/WaypointForm.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraBars;
+using DevExpress.XtraMap;
 
 namespace BorusanLogisticsGuzergah
 {
@@ -16,6 +17,19 @@ namespace BorusanLogisticsGuzergah
         public WaypointForm()
         {
             InitializeComponent();
+        }
+
+        private void WaypointForm_Load(object sender, EventArgs e)
+        {
+            mapControl1.Layers.Add(new ImageLayer()
+            {
+                DataProvider = new BingMapDataProvider()
+                {
+                    //https://www.bingmapsportal.com/Application#
+                    BingKey = "Avvc7Zi1mbEsmv7IRo9TnNbP32cLralhqFq3AhC-JsaVXS_qymj9GPT8TdOynshZ",
+                    Kind = BingMapKind.Road
+                }
+            });
         }
     }
 }
