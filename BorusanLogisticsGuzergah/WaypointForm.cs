c325ebb19b0e96a5b856359a804f74650dae510a
Mehmet Cenk Yeniköylü <mcy@dotnetmcy.net>
2021-08-31 13:39:07 +0300

    Create Bubbles

--- BorusanLogisticsGuzergah/WaypointForm.cs
+++ BorusanLogisticsGuzergah/WaypointForm.cs
@@ -30,6 +30,92 @@ namespace BorusanLogisticsGuzergah
                     Kind = BingMapKind.Road
                 }
             });
+            mapControl1.ZoomLevel = 6.0;
+            mapControl1.CenterPoint = new GeoPoint(39, 35);
+
+            // Create a layer to show vector items.
+            VectorItemsLayer itemsLayer = new VectorItemsLayer()
+            {
+                Data = CreateData(),
+                Colorizer = CreateColorizer()
+            };
+
+            mapControl1.Layers.Add(itemsLayer);
+
+            // Show a color legend.
+            mapControl1.Legends.Add(new ColorListLegend() { Layer = itemsLayer });
         }
+
+        #region #CreateBubbles
+        // Create a storage to provide data for the vector layer.
+        private IMapDataAdapter CreateData()
+        {
+            MapItemStorage storage = new MapItemStorage();
+
+            // Add Bubble charts with different values, sizes and 
+            // locations to the storage's Items collection.
+            storage.Items.Add(new MapBubble()
+            {
+                Argument = "A",
+                Value = 200,
+                Location = new GeoPoint(-45, -60),
+                Size = 20,
+                Group = 1,
+                MarkerType = MarkerType.Diamond
+            });
+            storage.Items.Add(new MapBubble()
+            {
+                Argument = "B",
+                Value = 400,
+                Location = new GeoPoint(-45, 0),
+                Size = 40,
+                Group = 2,
+                MarkerType = MarkerType.Plus
+            });
+            storage.Items.Add(new MapBubble()
+            {
+                Argument = "C",
+                Value = 800,
+                Location = new GeoPoint(-45, 60),
+                Size = 80,
+                Group = 1,
+                MarkerType = MarkerType.Cross
+            });
+            storage.Items.Add(new MapBubble()
+            {
+                Argument = "D",
+                Value = 800,
+                Location = new GeoPoint(-45, 90),
+                Size = 30,
+                Group = 3,
+                MarkerType = MarkerType.Circle
+            });
+
+            return storage;
+        }
+        #endregion #CreateBubbles
+
+        // Create a colorizer to provide colors for bubble items.    
+        private MapColorizer CreateColorizer()
+        {
+            KeyColorColorizer colorizer = new KeyColorColorizer();
+
+            // Add colors to the colorizer.
+            colorizer.Colors.Add(Color.Coral);
+            colorizer.Colors.Add(Color.Orange);
+            colorizer.Colors.Add(Color.LightBlue);
+            colorizer.Colors.Add(Color.OrangeRed);
+
+            colorizer.Keys.Add(new ColorizerKeyItem() { Key = "A", Name = "Category A" });
+            colorizer.Keys.Add(new ColorizerKeyItem() { Key = "B", Name = "Category B" });
+            colorizer.Keys.Add(new ColorizerKeyItem() { Key = "C", Name = "Category C" });
+            colorizer.Keys.Add(new ColorizerKeyItem() { Key = "D", Name = "Category D" });
+
+            // Load color indexes from bubbles via the 'Color' attribute
+            colorizer.ItemKeyProvider = new ArgumentItemKeyProvider();
+
+            return colorizer;
+        }
+
     }
 }
